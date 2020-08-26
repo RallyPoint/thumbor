a91cd3a567f99ba4e28c4a07a4df0d9baaf3bb9f
franck <franck.rst@gmail.com>
2020-08-26 10:36:09 +0200

    init

---
+++ ci/travis/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "--------------"
+echo $KUBE_NAMESPACE
+echo "--------------"
+
+function deploy_name() {
+    name="$CI_ENVIRONMENT_SLUG"
+    track="${1-stable}"
+
+    if [[ "$track" != "stable" ]]; then
+      name="$name-$track"
+    fi
+
+    echo $name
+}
+
+
+track="${1-stable}"
+name=$(deploy_name "$track")
+echo "KUBE_NAMESPACE: $KUBE_NAMESPACE";
+
+pwd
+
+kubectl create namespace $KUBE_NAMESPACE
+
+helm upgrade --install \
+  --wait \
+  --set env.HTTP_PORT="$HTTP_PORT" \
+  --set env.RTMP_PORT="$RTMP_PORT" \
+  --set env.HLS_API="$HLS_API" \
+  --namespace="$KUBE_NAMESPACE" \
+  "$name" \
+  ci/chart/ --force
+
+
+echo "END"
